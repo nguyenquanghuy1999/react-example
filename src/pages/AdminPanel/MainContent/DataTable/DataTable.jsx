68931c8de914dda8509d440092e8349c9d7ecca8
nguyenquanghuy1999 <nguyenquanghuy1999>
2026-04-08 23:35:41 +0700

    update

--- src/pages/AdminPanel/MainContent/DataTable/DataTable.jsx
+++ src/pages/AdminPanel/MainContent/DataTable/DataTable.jsx
@@ -18,10 +18,10 @@ function DataTable({ resource, onOpenForm }) {
 
     const { data: resourceData, isLoading } = resourceQuery;
 
-    const rowsWithId = resourceData?.map((row, index) => ({
-        id: row.id ?? index, // hoặc row._id hay trường duy nhất khác nếu có
-        ...row,
-    }));
+    // const rowsWithId = resourceData?.map((row, index) => ({
+    //     id: row.id ?? index, // hoặc row._id hay trường duy nhất khác nếu có
+    //     ...row,
+    // }));
 
     const [DraggableDialogOpen, setDraggableDialogOpen] = useState(false);
     const [data, setData] = useState(null);
@@ -88,7 +88,8 @@ function DataTable({ resource, onOpenForm }) {
         <Box sx={{ maxHeight: 400, width: '100%' }}>
             <DataGrid
                 key={resource}
-                rows={rowsWithId}
+                rows={resourceData}
+                getRowId={(row) => row.id}
                 columns={columns}
                 loading={isLoading}
                 initialState={{
